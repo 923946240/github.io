70a11a772dc2bb253eab04a81d6f9d8c566d49ae
923946240 <923946240@qq.com>
2016-05-06 11:23:17 +0800

    Added files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+﻿<?PHP include("php/V1.php");  ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>平哥云流量平台</title>
+<link href="css/zui.css" rel="stylesheet" type="text/css" />
+<link href="css/zui.lite.css" rel="stylesheet" type="text/css" />
+<link href="css/zui.lite.min.css" rel="stylesheet" type="text/css" />
+<link href="css/zui.min.css" rel="stylesheet" type="text/css" />
+<link href="css/zui-theme.css" rel="stylesheet" type="text/css" />
+<link href="css/zui-theme.min.css" rel="stylesheet" type="text/css" />
+<link href="css/css.css" rel="stylesheet" type="text/css" />
+<script src="js/zui.min.js"></script>
+<script src="js/doc.min.js"></script>
+<script src="js/jquery.js"></script>
+<script src="js/a.js"></script>
+<style type="text/css">
+<!--
+.STYLE1 {color: #000;font-family: "Microsoft YaHei","微软雅黑","Trebuchet MS",Arial,Verdana,Tahoma,sans-serif;}
+.STYLE2 {color: #FFF}
+-->
+</style>
+</head>
+
+<body>
+
+ <div class="bg-image-pattern"></div>
+<div align="center">  
+
+<img class="bg-image" src="http://limh.me/content/templates/limh.me/images/bg/7.jpg"> 
+ 
+ <div class="wrapper">
+ <img src='img/wifipng.png'width='50%'height='50'/>
+ <h1 class="STYLE1"> 平哥云流量平台</h1>
+<br/>	
+
+<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=100% height=86 src="http://music.163.com/outchain/player?type=2&id=31721353&auto=1&height=66"></iframe>
+
+
+<div id="x">
+
+
+            <div class="input-group">
+              <span class="input-group-addon"><i class="icon icon-user"></i></span>
+              <input type="text" name="u" id="u" value="" class="form-control" placeholder="帐号">
+              <span class="input-group-addon">会员帐号</span>
+            </div>
+			<BR/>
+			
+			<div class="input-group">
+              <span class="input-group-addon"><i class="icon icon-eye-open"></i></span>
+              <input type="text" name="p" id="p" value="" class="form-control" placeholder="密码">
+              <span class="input-group-addon">会员密码</span>
+            </div>
+         
+		 <br/>
+		 
+		 <button onclick="bba()" class="btn btn-success" type="button">》立即会员登录《</button>
+		 <button onclick="zc_a()" class="btn btn-primary" type="button">注册会员</button>
+		 <button onclick="alert('请联系客服：1156807298');" class="btn btn-primary" type="button">忘记密码</button>
+
+</div>
+
+ </div>
+</div align="center">
+
+
+
+
+ <div>
+ 
+   <div align="center" class="STYLE2">高速流量  |专业售后 | 快速效率 | 版权所有 <br/>Copyright ©2016 H-ui.net All Rights Reserved. <br/> 京ICP备1000000号 </div>
+ </div>
+
+</body>
+</html>
+
+
+
+
+
+
+ 
+
+
